19ec235c02c3b80d597184a85c6f74bf8f349e2e
MichaelConstable <constab4@uwm.edu>
2024-09-03 06:48:18 -0500

    reminder you can just put the scripts after bunlding lol

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -21,4 +21,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
+
+  
 );
+
+// src/main.js or src/App.jsx
